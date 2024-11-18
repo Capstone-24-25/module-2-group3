e06979f76207fc58851077e6505de85042c102ba
KasturiSharma <102628522+KasturiSharma@users.noreply.github.com>
2024-11-17 17:27:05 -0800

    did part of task 2

--- scripts/kasturi_test.R
+++ scripts/kasturi_test.R
@@ -1,1 +1,18 @@
+library(tidyverse)
+library(tidytext)
+library(gutenbergr)
+library(rvest)
+
 #test
+
+
+
+load("~/Documents/Fall 2024/PSTAT 197A/module-2-group3/data/claims-raw.RData")
+claims_raw %>% head()
+claims_clean %>% head()
+claims_clean$text_clean
+
+claims_bigrams <- claims_clean %>%
+  unnest_tokens(bigram, text_clean, token = "ngrams", n = 2) 
+
+claims_bigrams$bigram
